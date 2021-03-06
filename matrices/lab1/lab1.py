b47809029809a0fd3a3634d18c4217e3c01e2951
Pediatria <pediatria@MacBook-Pediatria.local>
2021-03-06 13:33:15 +0100

    add remaining functions

--- matrices/lab1/lab1.py
+++ matrices/lab1/lab1.py
@@ -1,25 +1,73 @@
 import numpy as np
 import time
 
-def ijk(first, second, SIZE):
-    sum = 0
-    multiply = np.zeros((size, size))
-    for i in range(0, SIZE):
-        for j in range(0, SIZE):
-            for k in range(0, SIZE):
-                sum += first[i][k]*second[k][j]
-            multiply[i][j] = sum
-            sum = 0
-    return 0
 
-sizes = [10, 100, 1000]
-algorithms = [ijk]
+# i -> m
+# j -> n
+# k -> l
+
+def jki(first, second, m, n, l):
+    multiply = np.zeros((m, n))
+    for j in range(0, n):
+        for k in range(0, l):
+            for i in range(0, n):
+                multiply[i][j] += first[i][k] * second[k][j]
+    return multiply
+
+
+def jik(first, second, m, n, l):
+    multiply = np.zeros((m, n))
+    for j in range(0, n):
+        for i in range(0, m):
+            for k in range(0, l):
+                multiply[i][j] += first[i][k] * second[k][j]
+    return multiply
+
+
+def kij(first, second, m, n, l):
+    multiply = np.zeros((m, n))
+    for k in range(0, l):
+        for i in range(0, m):
+            for j in range(0, n):
+                multiply[i][j] += first[i][k] * second[k][j]
+    return multiply
+
+
+def kji(first, second, m, n, l):
+    multiply = np.zeros((m, n))
+    for k in range(0, l):
+        for j in range(0, n):
+            for i in range(0, m):
+                multiply[i][j] += first[i][k] * second[k][j]
+    return multiply
+
+
+def ijk(first, second, m, n, l):
+    multiply = np.zeros((m, n))
+    for i in range(0, m):
+        for j in range(0, n):
+            for k in range(0, l):
+                multiply[i][j] += first[i][k] * second[k][j]
+    return multiply
+
+
+def ikj(first, second, m, n, l):
+    multiply = np.zeros((m, n))
+    for i in range(0, m):
+        for j in range(0, n):
+            for k in range(0, l):
+                multiply[i][j] += first[i][k] * second[k][j]
+    return multiply
+
+
+sizes = [10, 100, 200]
+algorithms = [ijk, ikj, kij, kji, jki, jik]
 
 for size in sizes:
     first = np.random.rand(size, size)
     second = np.random.rand(size, size)
     for algorithm in algorithms:
         start = time.time()
-        algorithm(first, second, size)
+        algorithm(first, second, size, size, size)
         stop = time.time()
         print(algorithm.__name__ + " / " + str(size) + ": " + str(stop - start))
